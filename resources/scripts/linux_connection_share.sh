4b55afb86d88e0b3f67c5256859710f190db0bc8
Héctor Molinero Fernández <hector@molinero.dev>
2021-04-28 19:16:54 +0200

    Automatically find network interfaces

--- resources/scripts/linux_connection_share.sh
+++ resources/scripts/linux_connection_share.sh
@@ -2,11 +2,10 @@
 
 set -eu
 
-USB_IFACE=${1:-enp7s0f4u2}
-USB_IFACE_NET=10.3.14.0/24
-UPSTREAM_IFACE=${2:-enp5s0}
+UPSTREAM_IFACE=${2:-$(awk '$2 == "00000000" {print($1); exit}' /proc/net/route)}
+USB_IFACE=${1:-$(awk '$2 == "000E030A" {print($1); exit}' /proc/net/route)}
 
-iptables -A FORWARD -o "${UPSTREAM_IFACE:?}" -i "${USB_IFACE:?}" -s "${USB_IFACE_NET:?}" -m conntrack --ctstate NEW -j ACCEPT
+iptables -A FORWARD -o "${UPSTREAM_IFACE:?}" -i "${USB_IFACE:?}" -s '10.3.14.0/24' -m conntrack --ctstate NEW -j ACCEPT
 iptables -A FORWARD -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 iptables -t nat -F POSTROUTING
 iptables -t nat -A POSTROUTING -o "${UPSTREAM_IFACE:?}" -j MASQUERADE
